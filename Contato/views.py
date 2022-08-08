f15de4fea81d0bb4f1c487e1ca4122c857bd6944
fat83dotcom <fernando.quimic@gmail.com>
2022-08-08 19:52:46 -0300

    termino dos preparativos dos templates

--- Contato/views.py
+++ Contato/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'contatos/index.html')
+    return render(request, 'contato/index.html')
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PROJETO_AGENDA
+
+-> Projeto iniciado em 7/8/22
+
