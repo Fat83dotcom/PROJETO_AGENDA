431e174ed6ebe35c15e45609ef6cc2f1560ec419
fat83dotcom <fernando.quimic@gmail.com>
2022-08-08 20:18:07 -0300

    criacao da base de dados

--- Contato/models.py
+++ Contato/models.py
@@ -1,3 +1,18 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=150)
+    sobrenome = models.CharField(max_length=100, blank=True)
+    telefone = models.CharField(max_length=20)
+    email = models.CharField(max_length=100, blank=True)
+    dt_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
