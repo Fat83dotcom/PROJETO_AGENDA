2e09a098cb376b41d3191ca83fe5898afdd79dff
fat83dotcom <fernando.quimic@gmail.com>
2022-08-09 20:28:00 -0300

    alterações na pagina admin

--- Contato/admin.py
+++ Contato/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Categoria, Contato
 
-# Register your models here.
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome', 'telefone', 'email')
+    list_display_links = ('telefone','nome')
+
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
--- Contato/models.py
+++ Contato/models.py
@@ -7,6 +7,9 @@ from django.db import models
 class Categoria(models.Model):
     nome = models.CharField(max_length=100)
 
+    def __str__(self) -> str:
+        return self.nome
+
 
 class Contato(models.Model):
     nome = models.CharField(max_length=150)
@@ -16,3 +19,6 @@ class Contato(models.Model):
     dt_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self) -> str:
+        return self.nome
