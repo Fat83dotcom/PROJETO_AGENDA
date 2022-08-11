ea79c680ef7ad748c4fb84a9fbfea78a34a38402
fat83dotcom <fernando.quimic@gmail.com>
2022-08-11 20:29:49 -0300

    levantando erros 404

--- Contato/views.py
+++ Contato/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Contato
+from django.http import Http404
 
 
 def index(request):
@@ -10,7 +11,11 @@ def index(request):
 
 
 def contatos(request, contato_id):
-    contato = Contato.objects.get(id=contato_id)
-    return render(request, 'contato/viewer_contato.html', {
-        'contato': contato
-    })
+    # contato = get_object_or_404(Contato, id=contato_id)  # maneira mais simples do que com o try
+    try:
+        contato = Contato.objects.get(id=contato_id)
+        return render(request, 'contato/viewer_contato.html', {
+            'contato': contato
+        })
+    except Contato.DoesNotExist:
+        raise Http404
