68c9c2750cae8aa8c124b39d8d01858223287735
fat83dotcom <fernando.quimic@gmail.com>
2022-08-26 21:26:07 -0300

    configuração de msgs de erro

--- Contato/views.py
+++ Contato/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render  # get_object_or_404
+from django.shortcuts import render, redirect  # get_object_or_404
 from .models import Contato
 from django.http import Http404
 from django.core.paginator import Paginator
+from django.contrib import messages
 
 
 def index(request):
@@ -9,7 +10,6 @@ def index(request):
     paginador = Paginator(contatos, 5)
     page = request.GET.get('p')
     contatos = paginador.get_page(page)
-
     return render(request, 'contato/index.html', {
         'contatos': contatos
     })
@@ -20,7 +20,11 @@ def contatos(request, contato_id):
     try:
         contato = Contato.objects.get(id=contato_id)
         if not contato.mostrar:
-            raise Http404
+            # raise Http404
+            messages.add_message(request,
+                                 messages.ERROR,
+                                 'O contato não pode ser exibido!')
+            return redirect('index')
 
         return render(request, 'contato/viewer_contato.html', {
             'contato': contato
